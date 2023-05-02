c7b0b1705a203424ca8c1271abe457d5697347a8
FilipeNamorato <filipennamorato@gmail.com>
2023-05-02 14:57:23 -0300

    Iniciando o projeto

---
+++ sistemaGestaoDiscente/src/main/java/com/mycompany/sistemagestaodiscente/SistemaGestaoDiscente.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemagestaodiscente;
+
+/**
+ *
+ * @author filipe
+ */
+public class SistemaGestaoDiscente {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
