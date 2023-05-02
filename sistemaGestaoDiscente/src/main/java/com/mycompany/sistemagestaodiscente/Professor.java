b1ea7ea136643f79d7a7c1e0ef82b7aea1e2c598
FilipeNamorato <filipennamorato@gmail.com>
2023-05-02 17:26:31 -0300

    Adicionando classes e alguns métodos correspondentes

---
+++ sistemaGestaoDiscente/src/main/java/com/mycompany/sistemagestaodiscente/Professor.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistemagestaodiscente;
+
+/**
+ *
+ * @author filipe
+ */
+public class Professor extends Usuario{
+    private String siape;
+     
+    public Professor(){
+        
+    }
+   
+    public String getSiape() {
+        return siape;
+    }
+    
+    private void setSiape(String siape) {
+        this.siape = siape;
+    }
+    
+    public void cadastroAlunoPorProfessor(){
+        
+    }
+    public void lancarNota(){
+        
+    }
+    public void lancarFrequencia(){
+        
+    }
+    public void consultaTurmas(){
+        
+    }
+    
+}
---
+++ sistemaGestaoDiscente/src/main/java/com/mycompany/sistemagestaodiscente/Usuario.java
@@ -0,0 +1,102 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.sistemagestaodiscente;
+/**
+ *
+ * @author filipe
+ */
+public class Usuario {
+    private int id;
+    private String nome;
+    private int CPF;
+    private String telefone;
+    private String rua;
+    private String bairro;
+    private String numero;
+    private String login;
+    private String senha;
+    private int tipoUsuario;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(int CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(int tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }    
+    
+}
