404ff8d73570783dfa8352d0a01f3dff8f958196
FilipeNamorato <filipennamorato@gmail.com>
2023-05-06 23:46:16 -0300

    Criando menu de interação via terminal.

--- sistemaGestaoDiscente/src/main/java/com/mycompany/sistemagestaodiscente/SistemaGestaoDiscente.java
+++ sistemaGestaoDiscente/src/main/java/com/mycompany/sistemagestaodiscente/SistemaGestaoDiscente.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.sistemagestaodiscente;
 
+import java.util.Scanner;
+
 /**
  *
  * @author filipe
@@ -11,6 +13,98 @@ package com.mycompany.sistemagestaodiscente;
 public class SistemaGestaoDiscente {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+        //Aluno aluno1 = new Aluno("Filipe",123,"123","filipe","123",2,"202165035AB");
+        Aluno vetAlunos[] = new Aluno[100];
+        int contAlunos = 0;
+        Professor vetProfessores[] = new Professor[10];
+        int contProfessor = 0;
+        
+        //aluno1.imprimeAluno();
+        Administrador adm = new Administrador("Filipe",123,"123","filipe","123",0,1);
+               
+        int opcao;
+        String nome,telefone, rua, bairro, numero, login, senha;
+        int CPF, CEP, tipoUsuario=2;
+        
+        do{
+            System.out.println("Bem vindo ao Sistema de Gestão Discente");
+            System.out.println("Digite: ");
+            System.out.println("0 para sair");
+            System.out.println("1 para cadastrar professor");
+            System.out.println("2 para cadastrar aluno");
+            System.out.println("3 para listar os alunos");
+            System.out.println("4 para listar os professores");
+            opcao = sc.nextInt();
+            
+            if(opcao==1){
+                
+                sc.nextLine();
+                System.out.println("Digite o nome:");
+                nome = sc.nextLine();
+                
+                System.out.println("Digite o CPF:");
+                CPF = sc.nextInt();
+                
+                System.out.println("Digite o telefone:");
+                telefone = sc.nextLine();
+                
+                sc.nextLine();
+                System.out.println("Digite o login:");
+                login = sc.nextLine();
+                
+                System.out.println("Digite a senha:");
+                senha = sc.nextLine();
+                
+                vetProfessores[contProfessor] = adm.cadastroProfessorPorAdm(nome, CPF, telefone, login, senha, 2);
+                contProfessor++;
+                
+            }else if(opcao==2){
+                
+                System.out.println("Digite o nome:");
+                nome = sc.nextLine();
+                
+                System.out.println("Digite o CPF:");
+                CPF = sc.nextInt();
+
+                System.out.println("Digite o telefone:");
+                telefone = sc.nextLine();
+                
+                System.out.println("Digite o login:");
+                login = sc.nextLine();
+
+                System.out.println("Digite a senha:");
+                senha = sc.nextLine();
+                
+                vetAlunos[contAlunos] = adm.cadastroAlunoPorAdm(nome, CPF, telefone, login, senha, 2);
+                contAlunos++;
+                
+            }else if(opcao==3){
+                for (int i = 0; i < contAlunos; i++) {
+                    if(vetAlunos[i]==null)
+                        break;
+                    System.out.println("--------Listando professores--------");
+                    System.out.println("Professor "+i+": ");
+                    vetAlunos[i].imprimeAluno();
+                    System.out.println("------------------------");
+                }
+
+                
+            }else if(opcao==4){
+                for (int i = 0; i < contProfessor; i++) {
+                    if(vetProfessores[i]==null)
+                        break;
+                    System.out.println("--------Listando professores--------");
+                    System.out.println("Professor "+i+": ");
+                    vetProfessores[i].imprimeProfessor();
+                    System.out.println("------------------------");
+                }
+                
+            }
+        
+        }while(opcao!=0);
+        
+        
+                
     }
 }
