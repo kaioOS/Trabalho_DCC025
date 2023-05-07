50e6df1c462a4a1b3bcd78c26a79e047119cd6da
kaioOS <kos20111@hotmail.com>
2023-05-07 13:41:20 -0300

    Criação da tela de login
    . Criação do pacote de telas e imagens
    . Ideia inicial para a tela de login
    . Adição da logo da UFJF nas imagens

---
+++ Telas/src/main/java/com/mycompany/telas/Telas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.telas;
+
+/**
+ *
+ * @author User
+ */
+public class Telas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
