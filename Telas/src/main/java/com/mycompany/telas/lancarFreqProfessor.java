415f1903c835df7782a5b8bef5371a1cbcd6d6ad
kaioOS <kos20111@hotmail.com>
2023-05-07 21:28:12 -0300

    Criação da tela de lançar Frequência do Professor
    .Tela de lançar nota do Professor
    .Correção na tela de visualização do Aluno

--- Telas/src/main/java/com/mycompany/telas/lancarFreqProfessor.java
+++ Telas/src/main/java/com/mycompany/telas/lancarFreqProfessor.java
@@ -8,12 +8,12 @@ package com.mycompany.telas;
  *
  * @author User
  */
-public class notaFreqAluno extends javax.swing.JFrame {
+public class lancarFreqProfessor extends javax.swing.JFrame {
 
     /**
-     * Creates new form notaFreqAluno
+     * Creates new form lancarFreqProfessor
      */
-    public notaFreqAluno() {
+    public lancarFreqProfessor() {
         initComponents();
     }
 
@@ -27,9 +27,11 @@ public class notaFreqAluno extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
         labelNomeAluno = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
         imgLogoUFJF = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
         jScrollPane3 = new javax.swing.JScrollPane();
         tableNotaFreq = new javax.swing.JTable();
 
@@ -37,29 +39,35 @@ public class notaFreqAluno extends javax.swing.JFrame {
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
 
-        jLabel1.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
-        jLabel1.setText("Matícula: 11112233344");
-
         labelNomeAluno.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
-        labelNomeAluno.setText("Aluno: Fulano da Silva");
+        labelNomeAluno.setText("Professor: Nome do Professor");
+
+        jLabel1.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
+        jLabel1.setText("Siape: 12345678");
 
         imgLogoUFJF.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagens/logoUFJF.png"))); // NOI18N
+
+        jLabel2.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
+        jLabel2.setText("Disciplina: Algoritmos");
+
+        jLabel3.setFont(new java.awt.Font("SansSerif", 1, 14)); // NOI18N
+        jLabel3.setText("Turma: A");
 
         tableNotaFreq.setFont(new java.awt.Font("Nunito", 0, 13)); // NOI18N
         tableNotaFreq.setForeground(new java.awt.Color(51, 51, 51));
         tableNotaFreq.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {"Algoritmos", "A",  new Integer(90), "75%", "7"},
-                {"Cálculo I", "B",  new Integer(60), "100%", "0"},
-                {"Química", "C",  new Integer(65), "50%", "15"},
-                {"Física", "A",  new Integer(50), "75%", "7"}
+                {"Fulano da Silva", "P", "P", "F", "F"},
+                {"Beltrano Ferreira", "F", "P", "P", "P"},
+                {"Ciclano de Souza", "P", "P", "P", "F"},
+                {"Maria Vieira", "P", "P", "P", "P"}
             },
             new String [] {
-                "Disciplina", "Tuma", "Nota", "Frequência", "Situação"
+                "", "25/01", "27/01", "01/02", "03/02"
             }
         ) {
             Class[] types = new Class [] {
-                java.lang.String.class, java.lang.String.class, java.lang.Integer.class, java.lang.String.class, java.lang.String.class
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
             };
 
             public Class getColumnClass(int columnIndex) {
@@ -81,39 +89,45 @@ public class notaFreqAluno extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 607, Short.MAX_VALUE)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(labelNomeAluno)
-                            .addComponent(jLabel1)
-                            .addComponent(imgLogoUFJF))
-                        .addGap(0, 0, Short.MAX_VALUE)))
-                .addContainerGap())
+                    .addComponent(imgLogoUFJF)
+                    .addComponent(labelNomeAluno)
+                    .addComponent(jLabel1)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel3))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addComponent(jScrollPane3, javax.swing.GroupLayout.DEFAULT_SIZE, 619, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(17, 17, 17)
                 .addComponent(imgLogoUFJF)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 107, Short.MAX_VALUE)
+                .addGap(64, 64, 64)
                 .addComponent(labelNomeAluno)
                 .addGap(4, 4, 4)
                 .addComponent(jLabel1)
-                .addGap(27, 27, 27)
-                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 124, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel3)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane3, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(25, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 6, Short.MAX_VALUE))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
@@ -136,20 +150,20 @@ public class notaFreqAluno extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(notaFreqAluno.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(lancarFreqProfessor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(notaFreqAluno.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(lancarFreqProfessor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(notaFreqAluno.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(lancarFreqProfessor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(notaFreqAluno.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(lancarFreqProfessor.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new notaFreqAluno().setVisible(true);
+                new lancarFreqProfessor().setVisible(true);
             }
         });
     }
@@ -157,6 +171,8 @@ public class notaFreqAluno extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel imgLogoUFJF;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane3;
     private javax.swing.JLabel labelNomeAluno;
