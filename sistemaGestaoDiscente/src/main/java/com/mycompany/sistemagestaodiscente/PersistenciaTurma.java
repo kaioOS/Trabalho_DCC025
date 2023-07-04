93fb530a2c6782df1970982d858d050baa1b809d
FilipeNamorato <filipennamorato@gmail.com>
2023-07-04 20:21:55 -0300

    refazendo lógica de persistência para turma

--- sistemaGestaoDiscente/src/main/java/com/mycompany/sistemagestaodiscente/PersistenciaTurma.java
+++ sistemaGestaoDiscente/src/main/java/com/mycompany/sistemagestaodiscente/PersistenciaTurma.java
@@ -5,21 +5,115 @@
 package com.mycompany.sistemagestaodiscente;
 
 import java.util.List;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.lang.reflect.Type;
+import com.google.gson.reflect.TypeToken;
+import java.io.IOException;
+import java.util.HashMap;
 
 /**
  *
  * @author filipe
  */
-public class PersistenciaTurma implements PersistenciaDados{
+public class PersistenciaTurma implements PersistenciaDados {
 
     @Override
-    public <T> void armazenarDados(List<T> objetos) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public <Turma> void armazenarDados(List<Turma> objetos) {
+
+        try {
+            File arquivo = new File("./src/main/java/com/mycompany/bancoDeDados/turmas.json");
+            FileWriter writer = new FileWriter(arquivo);
+
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+            gson.toJson(objetos, writer);
+
+            writer.flush();
+            writer.close();
+
+            System.out.println("Dados dos alunos armazenados com sucesso.");
+
+        } catch (IOException e) {
+            System.out.println("Erro ao armazenar os dados dos alunos: " + e.getMessage());
+        }
     }
 
     @Override
-    public <T> List<T> carregarDados() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    public List<Turma> carregarDados() {
+        List<Turma> turmas = new ArrayList<>();
+
+        Gson gson = new Gson();
+        File arquivo = new File("./src/main/java/com/mycompany/bancoDeDados/alunos.json");
+
+        if (arquivo.exists() && !arquivo.isDirectory()) {
+            try {
+                JsonReader reader = new JsonReader(new FileReader(arquivo));
+
+                JsonElement json = gson.fromJson(reader, JsonElement.class);
+
+                if (json != null) {
+                    if (json.isJsonArray()) {
+                        JsonArray array = json.getAsJsonArray();
+
+                        for (JsonElement element : array) {
+                            JsonObject turmaJson = element.getAsJsonObject();
+                            Turma turma = new Turma();
+
+                            // Desserializa os outros dados da turma do JSON
+                            turma.setCodigoTurma(turmaJson.get("codigoTurma").getAsString());
+                            turma.setProfessor(gson.fromJson(turmaJson.get("siapeProfessor"), Professor.class));
+                            turma.setDisciplina(gson.fromJson(turmaJson.get("codigoDisciplina"), Disciplina.class));
+                            turma.setFormaAvaliacao(turmaJson.get("formaAvaliacao").getAsInt());
+                            turma.defineFormaAvaliacao(turma.getFormaAvaliacao());
+
+                            // Desserializa a lista de alunos do JSON
+                            if (turmaJson.has("alunos")) {
+                                JsonArray alunosJsonArray = turmaJson.getAsJsonArray("alunos");
+                                List<Aluno> alunos = new ArrayList<>();
+
+                                for (JsonElement alunoElement : alunosJsonArray) {
+                                    JsonObject alunoJson = alunoElement.getAsJsonObject();
+                                    Aluno aluno = gson.fromJson(alunoJson, Aluno.class);
+                                    alunos.add(aluno);
+                                }
+
+                                turma.setAlunos(alunos);
+                            }
+
+                            // Desserializa o HashMap do turma do JSON
+                            if (turmaJson.has("hashMapJson")) {
+                                String hashMapJson = turmaJson.get("hashMapJson").getAsString();
+
+                                // Converte o JSON do HashMap de volta para um HashMap usando o GSON
+                                Type type = new TypeToken<HashMap<String, Integer>>() {
+                                }.getType();
+                                HashMap<String, Integer> hashMap = gson.fromJson(hashMapJson, type);
+
+                                // Adiciona o HashMap ao objeto Turma
+                                turma.setFrequencia(hashMap);
+                            }
+
+                            turmas.add(turma);
+                        }
+                    }
+                }
+
+                reader.close();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        return turmas;
     }
-    
+
 }
--- sistemaGestaoDiscente/src/main/java/com/mycompany/sistemagestaodiscente/Turma.java
+++ sistemaGestaoDiscente/src/main/java/com/mycompany/sistemagestaodiscente/Turma.java
@@ -12,22 +12,23 @@ import java.util.List;
  *
  * @author filipe
  */
-
 /**
-Kaio de Oliveira e Sousa(202165080AC)
-Filipe de Lima Namorato(202165035AB)
-Gustavo Silva Ribeiro (202165057AC) 
+ * Kaio de Oliveira e Sousa(202165080AC) Filipe de Lima Namorato(202165035AB)
+ * Gustavo Silva Ribeiro (202165057AC)
  */
-
 public class Turma {
-    private final String codigoTurma;  
+
+    private String codigoTurma;
     private Professor professor;
     private Disciplina disciplina;
     private List<Aluno> alunos;
-    private HashMap <String,Integer> frequenciaAlunos;
+    private HashMap<String, Integer> frequenciaAlunos;
     private int formaAvaliacao;
     private Avaliacao notaProvas;
-    
+
+    public Turma() {
+
+    }
 
     public Turma(String codigoTurma, Disciplina disciplina, int formaAvaliacao) throws ExceptionFormaAvaliacao, ExceptionTurmaCadastrada {
         validaFormaAvalicao(formaAvaliacao);
@@ -37,63 +38,69 @@ public class Turma {
         this.formaAvaliacao = formaAvaliacao;
         this.alunos = new ArrayList<>();
         this.frequenciaAlunos = new HashMap<>();
-        
-        if(this.formaAvaliacao == 1)
-        {
+
+        if (this.formaAvaliacao == 1) {
             notaProvas = new NotaMediaAritmetica();
         }
-        if(formaAvaliacao == 2)
-        {
-            notaProvas = new NotaMediaPonderada();          
+        if (this.formaAvaliacao == 2) {
+            notaProvas = new NotaMediaPonderada();
         }
-        if(formaAvaliacao == 3)
-        {
-            notaProvas = new NotaMediaComDescarte();      
+        if (this.formaAvaliacao == 3) {
+            notaProvas = new NotaMediaComDescarte();
         }
-        if(formaAvaliacao == 4)
-        {
-            notaProvas = new NotaSomatorio();      
+        if (this.formaAvaliacao == 4) {
+            notaProvas = new NotaSomatorio();
         }
-      
+
     }
-    
+
+    public void setFrequencia(HashMap<String, Integer> frequenciaAlunos) {
+        this.frequenciaAlunos = frequenciaAlunos;
+    }
+
     public String getCodigoTurma() {
         return codigoTurma;
     }
-    
+
+    public void setCodigoTurma(String codigoTurma) {
+        this.codigoTurma = codigoTurma;
+    }
+
     public Disciplina getDisciplina() {
         return disciplina;
     }
+
+    public void setDisciplina(Disciplina disciplina) {
+        this.disciplina = disciplina;
+    }
+
     public int getFormaAvaliacao() {
         return formaAvaliacao;
     }
-    
+
     public void setFormaAvaliacao(int formaAvaliacao) throws ExceptionFormaAvaliacao {
         validaFormaAvalicao(formaAvaliacao);
-        this.formaAvaliacao=formaAvaliacao;
+        this.formaAvaliacao = formaAvaliacao;
     }
-    
-    public void validaFormaAvalicao(int formaAvaliacao) throws ExceptionFormaAvaliacao{
-        if(formaAvaliacao!=1 && formaAvaliacao!=2 && formaAvaliacao!=3)
-        {
+
+    public void validaFormaAvalicao(int formaAvaliacao) throws ExceptionFormaAvaliacao {
+        if (formaAvaliacao != 1 && formaAvaliacao != 2 && formaAvaliacao != 3) {
             throw new ExceptionFormaAvaliacao();
         }
     }
-    
+
     public String getCodigoDisciplinaCorresp() {
         return this.disciplina.getCodigoDisciplina();
     }
-    
-    
+
     public String getSiapeProfessor() {
         return professor.getSiape();
     }
-    
+
     public void setProfessor(Professor professor) {
-       this.professor = professor;
+        this.professor = professor;
     }
-     
-    
+
     public void adicionarAluno(Aluno aluno) throws ExceptionTurma {
         if (this.disciplina.getCodigoDisciplina() != null) {
             alunos.add(aluno);
@@ -101,33 +108,36 @@ public class Turma {
             throw new ExceptionTurma();
         }
     }
-    
-        
-    public int getFrequenciaAluno(String matricula){
-        if(this.frequenciaAlunos.isEmpty())
+
+    public int getFrequenciaAluno(String matricula) {
+        if (this.frequenciaAlunos.isEmpty()) {
             return -1;
+        }
         return this.frequenciaAlunos.get(matricula);
     }
-    
-    
+
+    public void setAlunos(List<Aluno> alunos) {
+        this.alunos = alunos;
+    }
+
     //atualiza frequencia do aluno
-    public void setFrequenciaAluno(String matricula, boolean presenca){
+    public void setFrequenciaAluno(String matricula, boolean presenca) {
         for (Aluno aluno : alunos) {
             if (aluno.getMatricula().equals(matricula)) {
-                if(this.frequenciaAlunos.containsKey(matricula)){
-            if(presenca)
-                this.frequenciaAlunos.put(matricula, this.frequenciaAlunos.get(matricula)+1); 
-            }else{
-                if(presenca)
-                    this.frequenciaAlunos.put(matricula, 1);
-                else
-                    this.frequenciaAlunos.put(matricula, 0);
-            }
+                if (this.frequenciaAlunos.containsKey(matricula)) {
+                    if (presenca) {
+                        this.frequenciaAlunos.put(matricula, this.frequenciaAlunos.get(matricula) + 1);
+                    }
+                } else {
+                    if (presenca) {
+                        this.frequenciaAlunos.put(matricula, 1);
+                    } else {
+                        this.frequenciaAlunos.put(matricula, 0);
+                    }
+                }
                 System.out.println("Frequencia lançada para o aluno " + aluno.getNome() + " na disciplina " + this.getCodigoDisciplinaCorresp());
-            }
-            else
-            {
-               System.out.println("Matricula nao encontrada!");
+            } else {
+                System.out.println("Matricula nao encontrada!");
             }
         }
     }
@@ -135,9 +145,8 @@ public class Turma {
     public List<Float> getNotaAlunos(String matricula) {
         return this.notaProvas.getNotaAlunos().get(matricula);
     }
-    
-    public float getNotaFinalAluno(String matricula) throws ExceptionNota
-    {
+
+    public float getNotaFinalAluno(String matricula) throws ExceptionNota {
         return this.notaProvas.calcularMediaFinal(matricula);
     }
 
@@ -147,49 +156,61 @@ public class Turma {
             if (aluno.getMatricula().equals(matricula)) {
                 notaProvas.adicionarNota(aluno, notaAluno);
                 System.out.println("Nota lançada para o aluno " + aluno.getNome() + " na disciplina " + this.getCodigoDisciplinaCorresp());
+            } else {
+                System.out.println("Matricula nao encontrada!");
             }
-            else
-            {
-               System.out.println("Matricula nao encontrada!");
-            }
-        }   
-    }
-    
-    public void imprimeTurma() {
-        System.out.println("Codigo turma: "+this.codigoTurma);
-        System.out.println("Codigo disciplina: "+this.disciplina.getCodigoDisciplina());
-        if(this.professor!=null){
-            System.out.println("SIAPE professor: "+this.professor.getSiape());
         }
-        else
-        {
+    }
+
+    public void imprimeTurma() {
+        System.out.println("Codigo turma: " + this.codigoTurma);
+        System.out.println("Codigo disciplina: " + this.disciplina.getCodigoDisciplina());
+        if (this.professor != null) {
+            System.out.println("SIAPE professor: " + this.professor.getSiape());
+        } else {
             System.out.println("Professor nao cadastrado!");
         }
     }
+
     public void imprimirListaAlunos() {
-        System.out.println("Lista de Alunos da Disciplina "+ this.disciplina.getCodigoDisciplina() +" Turma " + this.codigoTurma);
+        System.out.println("Lista de Alunos da Disciplina " + this.disciplina.getCodigoDisciplina() + " Turma " + this.codigoTurma);
 
         for (Aluno aluno : alunos) {
             System.out.println("Nome: " + aluno.getNome());
             System.out.println("Matrícula: " + aluno.getMatricula());
         }
     }
-    
+
     public void imprimirProf() {
-        System.out.println("Professor da Disciplina "+ this.disciplina.getCodigoDisciplina() +" Turma " + this.codigoTurma);
+        System.out.println("Professor da Disciplina " + this.disciplina.getCodigoDisciplina() + " Turma " + this.codigoTurma);
         System.out.println("Nome: " + professor.getNome());
         System.out.println("Matrícula: " + professor.getSiape());
     }
-    public static void verificaTurma(String codigoTurma, Disciplina disciplina) throws ExceptionTurmaCadastrada{
+
+    public static void verificaTurma(String codigoTurma, Disciplina disciplina) throws ExceptionTurmaCadastrada {
         PersistenciaTurma persistenciaTurma = new PersistenciaTurma();
         List<Turma> turmas = persistenciaTurma.carregarDados();
         for (Turma turma : turmas) {
             if (turma.getCodigoTurma().equals(codigoTurma) && turma.getCodigoDisciplinaCorresp().equals(disciplina.getCodigoDisciplina())) {
-                    throw new ExceptionTurmaCadastrada();
+                throw new ExceptionTurmaCadastrada();
             }
         }
 
     }
 
-    
+    void defineFormaAvaliacao(int formaAvaliacao) {
+        if (formaAvaliacao == 1) {
+            notaProvas = new NotaMediaAritmetica();
+        }
+        if (formaAvaliacao == 2) {
+            notaProvas = new NotaMediaPonderada();
+        }
+        if (formaAvaliacao == 3) {
+            notaProvas = new NotaMediaComDescarte();
+        }
+        if (formaAvaliacao == 4) {
+            notaProvas = new NotaSomatorio();
+        }
+    }
+
 }
